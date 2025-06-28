19c0f99bf5a96d64fe071331d9ac0414d24433f8
ChuongBu <buingocchuong19022005@gmail.com>
2025-06-28 14:29:05 +0700

    up

---
+++ nvbang_2331540304/Assets/socorip_nVbangg/CameraFollow_NVBang.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollow_NVBang : MonoBehaviour
+{
+    public Transform target;          // Mario
+    public Vector3 offset;            // Khoảng cách lệch
+    public float smoothSpeed = 0.125f;
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        // Camera chỉ theo trục X, giữ nguyên Y, Z
+        transform.position = new Vector3(smoothedPosition.x, transform.position.y, -10f);
+    }
+}
